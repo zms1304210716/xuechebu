681c05b01b0e5802d013f838cf0ef51d968af86d
Administrator <1304210716@qq.com>
2019-11-26 18:45:42 +0800

    添加打印信息

--- tools/read_yaml.py
+++ tools/read_yaml.py
@@ -2,7 +2,7 @@ import yaml
 
 
 def build_login_data():
-    with open('../data/login_data.yaml', encoding='utf-8')as f:
+    with open('./data/login_data.yaml', encoding='utf-8')as f:
         data = yaml.safe_load(f)
         data_list = []
         for i in data.values():
